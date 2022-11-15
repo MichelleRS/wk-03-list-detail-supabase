35b9d9a4ba4675de4727afc1f34b7a9c3929f702
Michelle Ross <miross629@gmail.com>
2022-11-14 16:31:24 -0800

    Create fetch-utils with supabase connection and getAllHerbs()

---
+++ fetch-utils.js
@@ -0,0 +1,13 @@
+// connect to Supabase
+const SUPABASE_URL = 'https://qibydbafeplthyrhkgog.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpYnlkYmFmZXBsdGh5cmhrZ29nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwOTIsImV4cCI6MTk4MzY4NDA5Mn0.NrtQcuaIUkk_9z8xEvXMdOg-GqBQaeDdi3tGUQcDCQc';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getAllHerbs() {
+    // get all rows from herbs table
+    const response = await client.from('herbs').select('*');
+
+    return response.data;
+}
