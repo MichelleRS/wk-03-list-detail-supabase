8fe94ae8de09c577847e7e06956f5b3a4cf2a449
Michelle Ross <miross629@gmail.com>
2022-11-14 18:00:20 -0800

    addEventListener updated but not working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* Imports */
 
 import { getAllHerbs } from './fetch-utils.js';
+import { renderHerbCard } from './render-utils.js';
 
 /* Get DOM Elements */
 const herbsListContainer = document.querySelector('#herbs-list-container');
@@ -9,7 +10,14 @@ const herbsListContainer = document.querySelector('#herbs-list-container');
 
 /* Events */
 window.addEventListener('load', async () => {
+    // get all herbs
     const herbs = await getAllHerbs();
+
+    for (let herb of herbs) {
+        const herbEl = renderHerbCard(herb);
+
+        herbsListContainer.append(herbEl);
+    }
 });
 
 /* Display Functions */
